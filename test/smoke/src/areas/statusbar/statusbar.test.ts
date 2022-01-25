e990b2cbdc20f7672355adb09af22aa278f03bad
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-01-25 08:41:10 +0100

    fail test

--- test/smoke/src/areas/statusbar/statusbar.test.ts
+++ test/smoke/src/areas/statusbar/statusbar.test.ts
@@ -59,7 +59,7 @@ export function setup(isWeb: boolean, logger: Logger) {
 
 		it(`verifies that 'Problems View' appears when clicking on 'Problems' status element`, async function () {
 			const app = this.app as Application;
-			await app.workbench.statusbar.clickOn(StatusBarElement.PROBLEMS_STATUS);
+			//await app.workbench.statusbar.clickOn(StatusBarElement.PROBLEMS_STATUS);
 			await app.workbench.problems.waitForProblemsView();
 		});
 
